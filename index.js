87bd4520d0017e2b7283088daa54cbc0dd70c877
Farman Anees <farmananees831@gmail.com>
2025-05-04 00:46:28 +0500

    adding styling and javascript

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-function handleSubmit() {
-  
-    console.log("I am submitted");
-    return true
+ function handleSubmit() {
+
+    const firstName = document.getElementById("First Name").value;
+    const secondName = document.getElementById("Second Name").value;
+    const mobileNumber = document.getElementById("Mobile Number").value;
+    const email = document.getElementById("Email").value;
+    const message = document.getElementById("Message")
+
+
+
+    console.log("First Name:", firstName);
+    console.log("Second Name:", secondName);
+    console.log("Mobile Number:", mobileNumber);
+    console.log("Email:", email);
+    console.log("Message:", message);
+
+    alert(`Thank you, ${firstName}! Your message has been submitted.`);
+
+ 
+    document.querySelector("form").reset();
   }
